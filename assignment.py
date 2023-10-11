87291228c1e5538ccb5ca2d2cca9536c3849a15c
Joseph123231 <144241862+Joseph123231@users.noreply.github.com>
2023-10-11 19:49:21 -0400

    Verson where everything works except 2 tests

--- assignment.py
+++ assignment.py
@@ -1,1 +1,18 @@
-
+import math
+variable1 = float(input("what's the radius of your circle "))
+#this is where the formula and the variable make the area and circumference.
+area = variable1**2 * math.pi
+circumfrence = 2*math.pi*variable1
+#These next 4 if statements are conditions of the number inputed if the radius entered is a positive float you will get circumference and area
+if variable1 > 0:
+    print(" The area of your circle is ", area , " and the circumfrence is ", circumfrence,".")
+    #if a negative number is entered then you get message alerting your of your error
+if variable1 < 0:
+    print("Please enter a number that is POSITIVE")
+        #if ZERO is entered then you get message alerting your of your error as zero is not vaild for area and circumference
+if variable1 == 0:
+    print("Please use a number other then ZERO")
+    #if nothing is inputed then a message tells you to input a radius so the calculation can be done (NOT DONE) 
+if variable1 == None:
+    print("Please re-enter your radius and verify you have actuallt typed it")
+### Need to make a scenario for input = nothing and input = letters ###
