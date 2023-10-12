b28925933665ede9fde3c449281ece006eaec9ff
Joseph123231 <144241862+Joseph123231@users.noreply.github.com>
2023-10-12 14:27:08 -0400

    Try and Exception errors

--- assignment.py
+++ assignment.py
@@ -1,5 +1,8 @@
 import math
-variable1 = float(input("what's the radius of your circle "))
+try:
+    variable1 = float(input("what's the radius of your circle "))
+except ValueError : 
+    print("THIS SHOULD APPEAR IF THERE IS A VALUE ERROR")
 #this is where the formula and the variable make the area and circumference.
 area = variable1**2 * math.pi
 circumfrence = 2*math.pi*variable1
@@ -11,6 +14,8 @@ if variable1 < 0:
     print("Please enter a number that is POSITIVE")
         #if ZERO is entered then you get message alerting your of your error as zero is not vaild for area and circumference
 if variable1 == 0:
-    print("Please use a number other then ZERO")
-    #if nothing is inputed then a message tells you to input a radius so the calculation can be done (NOT DONE) 
+    print("Please use a number other then ZERO") 
+    #if variable1 = nothing then python finds error then excicutes the except once the error is found
+
+#if nothing is inputed then a message tells you to input a radius so the calculation can be done (NOT DONE YET) 
 ### Need to make a scenario for input = nothing and input = letters ###
