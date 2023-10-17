d95a9d84d044c965e393928e7ab95c753e60df51
Joseph123231 <144241862+Joseph123231@users.noreply.github.com>
2023-10-17 09:00:34 -0400

    All test cases work

--- assignment.py
+++ assignment.py
@@ -1,21 +1,15 @@
 import math
 try:
-    variable1 = float(input("what's the radius of your circle "))
-except ValueError : 
-    print("THIS SHOULD APPEAR IF THERE IS A VALUE ERROR")
-#this is where the formula and the variable make the area and circumference.
-area = variable1**2 * math.pi
-circumfrence = 2*math.pi*variable1
-#These next 4 if statements are conditions of the number inputed if the radius entered is a positive float you will get circumference and area
-if variable1 > 0:
-    print(" The area of your circle is ", area , " and the circumfrence is ", circumfrence,".")
-    #if a negative number is entered then you get message alerting your of your error
-if variable1 < 0:
-    print("Please enter a number that is POSITIVE")
-        #if ZERO is entered then you get message alerting your of your error as zero is not vaild for area and circumference
-if variable1 == 0:
-    print("Please use a number other then ZERO") 
-    #if variable1 = nothing then python finds error then excicutes the except once the error is found
+    variable1 = float(input("what's the radius of your circle: "))
+    area = variable1**2 * math.pi
+    circumfrence = 2*math.pi*variable1
+except ValueError :
+    print("Verify you actually typed a NUMBER")
 
-#if nothing is inputed then a message tells you to input a radius so the calculation can be done (NOT DONE YET) 
-### Need to make a scenario for input = nothing and input = letters ###
+else:
+    if variable1 < 0:
+        print("Please enter a number that is POSITIVE")
+    if variable1 == 0:
+        print("Please use a number other then ZERO")
+    if variable1 > 0:
+        print(f"Your circumferemce is {circumfrence} and your area is {area}.")
