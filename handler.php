58810cccaaea36bbf3e5471fadf18c0b71a1b225
Joseph123231 <144241862+Joseph123231@users.noreply.github.com>
2023-10-17 11:37:57 -0400

    Update handler.php

--- handler.php
+++ handler.php
@@ -3,7 +3,7 @@
   <head>
     <title>Welcome Page</title>
   </head>
-  <form action="handler.php" method="Post">
+  <form action="handler.php" method="Post" name="userName">
   <div class="mdl-textfield mdl-js-textfield">
     <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
     <label class="mdl-textfield__label" for="sample2">Number...</label>
@@ -18,12 +18,14 @@
     if ( isset( $_POST['radius'] ) ){
       $radius = $_POST['radius'];
     }
+    $radius = ""
     $pi = 3.14
     $Area = pi * $radius ** 2
-    $circumference = 2 * $pi *
+    $circumference = 2 * $pi * $radius
+  
     echo "<h1>For a Circle of Radius ".$radius."</h1>\n";
     echo "<p>Area = $Area \n";
-    echo "<p>Circumference = \n";
+    echo "<p>Circumference =  $circumference \n";
     ?>
     <?php
     echo "hello world"
