392aceb1de8ca9c1768b691090406d55456d729a
Joseph123231 <144241862+Joseph123231@users.noreply.github.com>
2023-10-20 09:05:12 -0400

    Update handler.php

--- handler.php
+++ handler.php
@@ -3,34 +3,20 @@
   <head>
     <title>Welcome Page</title>
   </head>
-  <form action="handler.php" method="Post" name="userName">
-  <div class="mdl-textfield mdl-js-textfield">
-    <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">
-    <label class="mdl-textfield__label" for="sample2">Number...</label>
-    <span class="mdl-textfield__error">Input is not a number!</span>
-  </div>
-</form>
   <body>
-        
+
     <form>
     <?php
-    $userName = "";
-    if ( isset( $_POST['radius'] ) ){
-      $radius = $_POST['radius'];
+    if ( isset( $_POST['userName'] ) ){
+      $radius = $_POST['userName'];
     }
-    $radius = ""
-    $pi = 3.14
-    $Area = pi * $radius ** 2
-    $circumference = 2 * $pi * $radius
-  
+    $Area = 3.14 * ($radius ** 2);
+    $circumference = 2 * 3.14 *$radius;
     echo "<h1>For a Circle of Radius ".$radius."</h1>\n";
-    echo "<p>Area = $Area \n";
-    echo "<p>Circumference =  $circumference \n";
-    ?>
-    <?php
-    echo "hello world"
+    echo "<p>Area = ".$Area." \n";
+    echo "<p>Circumference = ".$circumference." \n";
     ?>
     </form>
   </body>
-  
+
 </html>
