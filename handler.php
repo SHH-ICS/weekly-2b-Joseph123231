f0309920b71ff752557f832656f62c2d00470b08
Joseph123231 <144241862+Joseph123231@users.noreply.github.com>
2023-10-25 12:26:53 +0000

    finl changes

--- handler.php
+++ handler.php
@@ -7,19 +7,18 @@
 
     <form>
     <?php
-var_dump($_POST);
     if ( isset( $_POST['radius'] ) ){
-      $radius = $_POST['radius'];
+    $radius = $_POST['radius'];
     }
     $Area = 3.14 * ($radius ** 2);
     $circumference = 2 * 3.14 *$radius;
-        if ($radius < 0){
-                 echo "can't calculate you radius because you entered a negative number";
-        }else {
+        if ($radius >= 0){
          echo "<h1>For a Circle of Radius ".$radius."</h1>\n";
          echo "<p>Area = ".$Area." \n";
          echo "<p>Circumference = ".$circumference." \n";
-        };
+        }else {
+        echo  "<h2> YOUR INPUT IS NOT VALID!!! </h2>\n";
+        }
     ?>
     </form>
   </body>
